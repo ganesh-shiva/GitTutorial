0b7612288c1dcb963cd22d43ccd1da0d9ceb73e8
Ganesh Shiva <30925154+ganesh-shiva@users.noreply.github.com>
2017-09-11 01:03:40 +0530

    Update HelloWorld.java
    Edited HelloWorld.java file and will try pulling the code from client.

--- src/com/java/example/HelloWorld.java
+++ src/com/java/example/HelloWorld.java
@@ -6,6 +6,8 @@ public class HelloWorld {
 		System.out.println("Hi there!!");
 		System.out.println("This program's written as part of git learning");
 		System.out.println("Ok then, bye.");
+		System.out.prontln("--------------------");
+		System.out.println("Added this line from github editor");
 	}
 
 }

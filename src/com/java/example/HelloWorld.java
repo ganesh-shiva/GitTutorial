d9943e2fad68d4288731a428a77430b1c3e7a8bc
ganesh-shiva <ganesh.shiva@wipro.com>
2017-09-11 01:11:03 +0530

    Fixed the compilation error. Now Committing the code

--- src/com/java/example/HelloWorld.java
+++ src/com/java/example/HelloWorld.java
@@ -6,7 +6,7 @@ public class HelloWorld {
 		System.out.println("Hi there!!");
 		System.out.println("This program's written as part of git learning");
 		System.out.println("Ok then, bye.");
-		System.out.prontln("--------------------");
+		System.out.println("--------------------");
 		System.out.println("Added this line from github editor");
 	}
 

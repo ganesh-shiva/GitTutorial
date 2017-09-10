6e5f0f8bdf28043ab976fa4b61f13fd8af0aed23
ganesh-shiva <ganesh.shiva@wipro.com>
2017-09-11 00:50:31 +0530

    First Commit

---
+++ src/com/java/example/HelloWorld.java
@@ -0,0 +1,11 @@
+package com.java.example;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		System.out.println("Hi there!!");
+		System.out.println("This program's written as part of git learning");
+		System.out.println("Ok then, bye.");
+	}
+
+}
